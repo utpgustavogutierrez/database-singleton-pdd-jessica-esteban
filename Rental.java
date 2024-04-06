45f30e143100a736af1d79a9d7b7cfbdf2c40949
estebanlopezsu <159578693+estebanlopezsu@users.noreply.github.com>
2024-04-06 17:51:19 -0500

    Create Rental.java

---
+++ Rental.java
@@ -0,0 +1,9 @@
+public class Rental(){
+    private int rental_id
+    private String rental_date
+    private int inventory_id
+    private int customer_id
+    private String return_date
+    private int staff_id
+
+}

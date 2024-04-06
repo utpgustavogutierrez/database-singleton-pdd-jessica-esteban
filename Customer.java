6ef6b62f977d4bc781eecbf68dce4e34755fe59b
estebanlopezsu <159578693+estebanlopezsu@users.noreply.github.com>
2024-04-06 17:48:10 -0500

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,11 @@
+public class Customer(){
+    private int customer_id
+    private int store_id
+    private String first_name
+    private String last_name
+    private String email
+    private String addres_id
+    private String active
+    private Sring create_date
+
+}

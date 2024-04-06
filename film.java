9cc0ef20d63d9e8f93ee82bc5a0120b67c4a484f
estebanlopezsu <159578693+estebanlopezsu@users.noreply.github.com>
2024-04-06 17:49:26 -0500

    Create film.java

---
+++ film.java
@@ -0,0 +1,13 @@
+public class Film(){
+    private String title
+    private int film_id
+    private Date release_year
+    private int rental_diration
+    private String lengage_id
+    private int rental_rate
+    private int length
+    private int replacement_cost
+    private String rating
+    private String special_features
+
+}

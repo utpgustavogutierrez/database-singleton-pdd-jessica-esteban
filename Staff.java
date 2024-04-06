09e925827c465be0747ad219ade40493ef457e70
estebanlopezsu <159578693+estebanlopezsu@users.noreply.github.com>
2024-04-06 17:51:40 -0500

    Create Staff.java

---
+++ Staff.java
@@ -0,0 +1,12 @@
+public class Staff(){
+    private int staff_id
+    private String first_name
+    private String last_name
+    private int address_id
+    private String email
+    private int store_id
+    private String active
+    private String username
+    private String password
+
+}

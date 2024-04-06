f462699ab1247310775bd46d74e1d3f1f5c18b2c
estebanlopezsu <159578693+estebanlopezsu@users.noreply.github.com>
2024-04-06 17:50:52 -0500

    Create Payment.java

---
+++ Payment.java
@@ -0,0 +1,10 @@
+public class Payment(){
+    private int payment_id
+    private int customer_id
+    private int staff_id
+    private int rental_id
+    private String amount
+    private Date payment_date
+
+}
+

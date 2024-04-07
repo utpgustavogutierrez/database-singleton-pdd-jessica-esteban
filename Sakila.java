bb3909cc9523d143e2c756f2f16dc8d43b979c62
Jessica Alexandra Giraldo López <125520069+alexandra14090@users.noreply.github.com>
2024-04-07 14:34:53 -0500

    Update Sakila.java

--- Sakila.java
+++ Sakila.java
@@ -4,33 +4,53 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Sakila
-{
-  public static void main(String[] args)
-  {
-    // NOTE: Connection and Statement are AutoClosable.
-    //       Don't forget to close them both in order to avoid leaks.
-    try
-    (
-      // create a database connection
-      Connection connection = DriverManager.getConnection("jdbc:sqlite:./sqlite-sakila.db");
-      Statement statement = connection.createStatement();
-    )
-    {
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
+public class DatabaseSingleton {
+    private static DatabaseSingleton instance;
+    private Connection connection;
 
-      ResultSet rs = statement.executeQuery("select * from film");
-      while(rs.next())
-      {
-        // read the result set
-        System.out.println("title = " + rs.getString("title"));
-      }
+    private DatabaseSingleton() {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:./sqlite-sakila.db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    catch(SQLException e)
-    {
-      // if the error message is "out of memory",
-      // it probably means no database file is found
-      e.printStackTrace(System.err);
+    // DatabaseSingleton es nuestro patrón Singleton
+    // Es una creación de una única instancia de conexión de base de datos que se puede compartir en toda la aplicación.
+    public static DatabaseSingleton getInstance() {
+        if (instance == null) {
+            instance = new DatabaseSingleton();
+        }
+        return instance;
     }
-  }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
+
+public class Sakila {
+    public static void main(String[] args) {
+        try {
+            // Obtener la instancia de la conexión de la base de datos
+            DatabaseSingleton databaseSingleton = DatabaseSingleton.getInstance();
+            Connection connection = databaseSingleton.getConnection();
+
+            // NOTE: Connection and Statement are AutoClosable.
+            //       Don't forget to close them both in order to avoid leaks.
+            try (Statement statement = connection.createStatement()) {
+                statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+                ResultSet rs = statement.executeQuery("select * from film");
+                while (rs.next()) {
+                    // read the result set
+                    System.out.println("title = " + rs.getString("title"));
+                }
+            }
+        } catch (SQLException e) {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            e.printStackTrace(System.err);
+        }
+    }
 }
